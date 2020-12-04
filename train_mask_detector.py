3f5bed93c63f447ebf59ae2f308b408e9dd6a26a
Nico Tanzil <63032763+nicotanzil@users.noreply.github.com>
2020-12-04 15:29:37 +0700

    Adding screen capture if face with no mask is detected

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -1,24 +1,24 @@
 # import the necessary packages
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
 from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Input
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.utils import to_categorical
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from imutils import paths
-import matplotlib.pyplot as plt
-import numpy as np
-import os
 
 # initialize the initial learning rate, number of epochs to train for,
 # and batch size
@@ -26,7 +26,7 @@ INIT_LR = 1e-4
 EPOCHS = 20
 BS = 32
 
-DIRECTORY = r"D:\Nico\4. Sem 3\5. AI\Project_FaceMask_2\dataset"
+DIRECTORY = r"D:\Nico\4. Sem 3\5. AI\Project_FaceMask\dataset"
 CATEGORIES = ["with_mask", "without_mask"]
 
 # grab the list of images in our dataset directory, then initialize
@@ -83,7 +83,7 @@ headModel = Dropout(0.5)(headModel)
 headModel = Dense(2, activation="softmax")(headModel)
 
 # place the head FC model on top of the base model (this will become
-# the actual model we will train)
+# # the actual model we will train)
 model = Model(inputs=baseModel.input, outputs=headModel)
 
 # loop over all layers in the base model and freeze them so they will
